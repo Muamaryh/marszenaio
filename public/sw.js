aaa6117822a90981ff23018f48fd8e5b340833f0
Muamaryh <muamaryh26@gmail.com>
2026-08-23 18:44:38 +0700

    fix(subtitle): keep subtitle permanently visible when video player controls auto-hide

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * MARSZTREAM - Service Worker (PWA Shell Caching)
  */
 
-const CACHE_NAME = 'marsztream-pwa-v13';
+const CACHE_NAME = 'marsztream-pwa-v14';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
