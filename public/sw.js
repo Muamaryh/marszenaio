06d3b2d72faee6ef750855c61068a4666942f1b7
Muamaryh <muamaryh26@gmail.com>
2026-08-23 19:57:11 +0700

    feat(providers): align exact 19 active documentation sources and remove dramaboxv2

--- public/sw.js
+++ public/sw.js
@@ -2,7 +2,7 @@
  * MARSZTREAM - Service Worker (PWA Shell Caching)
  */
 
-const CACHE_NAME = 'marsztream-pwa-v21';
+const CACHE_NAME = 'marsztream-pwa-v22';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
